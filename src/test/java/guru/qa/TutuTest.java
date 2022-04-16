9d5ab838e073dc01493df32e971beb99e4763aab
user <stel-vd@yandex.ru>
2022-04-16 16:57:16 +0300

    Added check-up  and renamed method parameters

--- src/test/java/guru/qa/TutuTest.java
+++ src/test/java/guru/qa/TutuTest.java
@@ -3,6 +3,7 @@ package guru.qa;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,15 +24,16 @@ public class TutuTest {
     @CsvSource({
             "Москва, Казань, 18, 21"
     })
+    @DisplayName("Тест на проверку наличия билетов по маршруту: {0}-{1}- с датами:  {2}-{3}")
     @ParameterizedTest(name = "Тест на проверку наличия билетов по маршруту: {0}-{1}- с датами:  {2}-{3}")
-    void findPriceTest(String first, String second, String third, String fourth) {
+    void findPriceTest(String cityfrom, String cityTo, String dateFrom, String dateTo) {
         open(baseUrl);
-        $(".input_field.j-city_input.j-city_from.j-main").setValue(first).pressEnter();
-        $(".input_field.j-city_input.j-city_to.j-main").setValue(second).pressEnter();
+        $(".input_field.j-city_input.j-city_from.j-main").setValue(cityfrom).pressEnter();
+        $(".input_field.j-city_input.j-city_to.j-main").setValue(cityTo).pressEnter();
         $(".j-date_from").shouldBe(Condition.visible).click();
-        $x("//a[contains(text(),'" + third +"')]").click();
+        $x("//a[contains(text(),'" + dateFrom +"')]").click();
         $(".j-date_back").shouldBe(Condition.visible).click();
-        $x("//a[contains(text(),'" + fourth +"')]").click();
+        $x("//a[contains(text(),'" + dateTo +"')]").click();
         $(".button_wrp.j-buttons_block").click();
         $x("//span[contains(text(), 'предлож')]").shouldHave(text("предлож"));
     }
@@ -41,15 +43,15 @@ public class TutuTest {
             "Москва, Казань, 18"
     })
     @ParameterizedTest(name = "Тест на проверку наличия билетов по маршруту: {0}-{1}- на дату:  {2}")
-    void findPriceTestOneWayTicket(String first, String second, String third) {
+    void findPriceTestOneWayTicket(String cityfrom, String cityTo, String dateFrom) {
         open(baseUrl);
-        $(".input_field.j-city_input.j-city_from.j-main").setValue(first).pressEnter();
-        $(".input_field.j-city_input.j-city_to.j-main").setValue(second).pressEnter();
+        $(".input_field.j-city_input.j-city_from.j-main").setValue(cityfrom).pressEnter();
+        $(".input_field.j-city_input.j-city_to.j-main").setValue(cityTo).pressEnter();
         $(".j-date_from").shouldBe(Condition.visible).click();
-        $x("//a[contains(text(),'" + third +"')]").click();
+        $x("//a[contains(text(),'" + dateFrom +"')]").click();
         //При запуске на IOS без слипа тест отображается как пройденный, но фактически в хроме не происходит нажатия
         // на кнопку! Не нашла способа кроме слип. На машине с WINDWS тест отрабатывает без слип.
-        //sleep(500);
+        sleep(500);
         $(".button_wrp.j-buttons_block").click();
         $x("//span[contains(text(), 'предлож')]").shouldHave(text("предлож"));
     }
@@ -59,16 +61,17 @@ public class TutuTest {
             "Москва, Казань, 18, 21",
             "Москва, Самара, 21, 25"
     })
-    @ParameterizedTest(name = "Тест на проверку наличия билетов по маршруту: {0}-{1}- с датами:  {2}-{3}")
-    void findPriceTest2(String first, String second, String third, String fourth) {
+    @DisplayName("Тест на проверку наличия билетов по маршруту: {0}-{1} с датами:  {2}-{3}")
+    @ParameterizedTest(name = "Тест на проверку наличия билетов по маршруту: {0}-{1} с датами:  {2}-{3}")
+    void findPriceTest2(String cityfrom, String cityTo, String dateFrom, String dateTo) {
         open(baseUrl);
         Configuration.holdBrowserOpen = true;
-        $(".input_field.j-city_input.j-city_from.j-main").setValue(first).pressEnter();
-        $(".input_field.j-city_input.j-city_to.j-main").setValue(second).pressEnter();
+        $(".input_field.j-city_input.j-city_from.j-main").setValue(cityfrom).pressEnter();
+        $(".input_field.j-city_input.j-city_to.j-main").setValue(cityTo).pressEnter();
         $(".j-date_from").shouldBe(Condition.visible).click();
-        $x("//a[contains(text(),'" + third +"')]").click();
+        $x("//a[contains(text(),'" + dateFrom +"')]").click();
         $(".j-date_back").shouldBe(Condition.visible).click();
-        $x("//a[contains(text(),'" + fourth +"')]").click();
+        $x("//a[contains(text(),'" + dateTo +"')]").click();
         sleep(1000);
         $(".button_wrp.j-buttons_block").click();
         $x("//span[contains(text(), 'предлож')]").shouldHave(text("предлож"));
